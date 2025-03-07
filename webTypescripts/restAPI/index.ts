68f09a20613a543b6f9a26336ad1174c78c08d7c
davho733 <davho733@student.liu.se>
2025-03-07 17:39:52 +0100

    test

---
+++ webTypescripts/restAPI/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Test endpoint
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Shrek is love, Shrek is life', status: 'online', ogre: true });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://207.180.233.244:${PORT}`);
+});
